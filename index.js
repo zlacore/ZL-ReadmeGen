1a51dc9fdfbb9faab2262ca5b91cec6d0fe8d055
Zachary LaCore <zlacore12@gmail.com>
2024-10-20 17:56:55 -0500

    Finished README Generator

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
 // TODO: Include packages needed for this application
 import inquirer from "inquirer";
-import generateMarkdown from "./utils/generateMarkdown";
-import Choices from "inquirer/lib/objects/choices";
+import generateMarkdown from "./utils/generateMarkdown.js";
+// import Choices from "inquirer/lib/objects/choices.js";
+import fs from "fs";
+
+
+function init() { }
+console.log("Beep boop beep boop, README generator loading!")
+// Function call to initialize app
+init();
+
+
+const data = {
+    title: "title",
+    description: "description",
+    install: "Installation",
+    usage: "Usage Information",
+    contribution: "Contribution Guidelines",
+    license: "License",
+    questions: "Questions",
+}
 // TODO: Create an array of questions for user input
 const questions = [{
     type: "input",
@@ -37,18 +55,43 @@ const questions = [{
 {
     type: "list",
     message: "Which license are you using for your product?",
-    choices: ["MIT", "ATF", "CIA", "FBI", "FML"],
+    choices: ["None", "MIT", "WTFPL", "ISC"],
     name: "license"
+},
+{
+    type: "input",
+    message: "For further inquiry, what is your github?",
+    name: "github"
+},
+{
+    type: "input",
+    message: "For further inquiry, what is your email?",
+    name: "email"
 }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) { }
+function writeToFile() {
+    inquirer
+    .prompt(questions)
+    .then((response) => {
+        const license = response.license
+            data.title = response.title;
+            data.description = response.description;
+            data.install = response.install;
+            data.usage = response.usage;
+            data.contribution = response.contribution;
+            data.tests = response.tests;
+            data.questions = 
+            `Github: ${response.github}         Email: ${response.email}`;
+            const markdown = generateMarkdown(data, license)
+            fs.writeFile('README.md', markdown, (err) => {
+                err ? console.log("Error!") : console.log("Yay!");
+            })
+        })
+}
+
+writeToFile();
 
 // TODO: Create a function to initialize app
-function init() { }
-
-// Function call to initialize app
-init();
-
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,107 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+const data = {
+  title: "title",
+  description: "description",
+  install: "Installation",
+  usage: "Usage Information",
+  contribution: "Contribution Guidelines",
+  license: "License",
+  github: "Questions",
+}
+
+function renderLicenseBadge(license) {
+  if (license === "None") {
+    return "";
+  } else if (license === "MIT") {
+    return "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "WTFPL") {
+    return "[![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)](http://www.wtfpl.net/about/)";
+  } else if (license === "ISC") {
+    return "[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)"
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { 
+  if (license === "None") {
+    return "";
+  } else if (license === "MIT") {
+    return "https://opensource.org/licenses/MIT"
+  } else if (license === "WTFPL") {
+    return "http://www.wtfpl.net/about/"
+  } else if (license === "ISC") {
+    return "https://opensource.org/licenses/ISC"
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license){ 
+  if (license === "None") {
+    return "This project has no license!";
+  } else if (license === "MIT") {
+    return "MIT License Copyright (c) [year] [fullname] Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the Software), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions: The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED AS IS, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. "
+  } else if (license === "WTFPL") {
+    return "DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE Version 2, December 2004 Copyright (C) 2004 Sam Hocevar evar.net> Everyone is permitted to copy and distribute verbatim or modified copies of this license document, and changing it is allowed as long as the name is changed. DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 0. You just DO WHAT THE FUCK YOU WANT TO. "
+  } else if (license === "ISC") {
+    return `Copyright YEAR OWNER Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.THE SOFTWARE IS PROVIDED AS IS AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.`
+  }
+}
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(data, license) {
+return `# ${data.title} ${renderLicenseBadge(license)}
+
+
+## Description
+
+> ${data.description}
+
+## Table of Contents
+> [Installation](#Installation-instructions)
+> [Usage](#usage-information)
+> [Contribution](#contribution-guidelines)
+> [Tests](#tests)
+> [License](#license)
+> [Questions](#questions)
+
+
+## Installation Instructions
+
+> ${data.install}
+
+
+
+## Usage Information
+
+> ${data.usage}
+
+
+
+## Contribution Guidelines
+
+> ${data.contribution}
+
+
+
+## Tests
+
+> ${data.tests}
+
+
+## License 
+${renderLicenseLink(license)}
+> ${renderLicenseSection(license)}
+
+
+## Questions
+
+> ${data.questions}
+
+
 
 `;
 }
